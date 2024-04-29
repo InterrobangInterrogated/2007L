066a8e3eaf41b8c121030243933313250827a032
Adam Moran <noears6@gmail.com>
2024-04-29 14:49:31 -0700

    Added missing files from 2008M client (https://github.com/MaximumADHD/Roblox-2008-Client)

---
+++ content/scripts/CSMPBoot.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:54a19da0b40fc37c7b1557692683bd26bbdc1f675aec4e6ebab913f54fac908c
+size 83
---
+++ content/scripts/CSMPFunctions.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:847e5b64e25dfc4a5a30188c6c264b52c7df55fb463edd24ebc072dc8afd4824
+size 18799
